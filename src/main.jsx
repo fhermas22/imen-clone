401f6b09181a08a22791bfb61ee8611fb0d042d4
Hermas Francisco <franciscohermas@gmail.com>
2026-02-10 15:28:47 +0100

    Configure global theme (main colors & fonts)

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,9 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
+import Home from './pages/Home/index.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <Home />
   </StrictMode>,
 )
---
+++ src/pages/Home/index.jsx
@@ -0,0 +1,10 @@
+
+function Home() {
+  return (
+    <>
+      <h1 className="font-title text-primary text-2xl font-semibold">IMeN : Institut des Métiers du Numérique</h1>
+    </>
+  )
+}
+
+export default Home
