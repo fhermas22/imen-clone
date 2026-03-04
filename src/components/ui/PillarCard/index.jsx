d6895234cfb06b27e2384dab9b6c06700e2ca752
Hermas Francisco <franciscohermas@gmail.com>
2026-03-04 09:19:48 +0100

    Created OurPillars section from Home page; (work in progress) Create PilllarCard component;

--- src/components/ui/PillarCard/index.jsx
+++ src/components/ui/PillarCard/index.jsx
@@ -1,10 +1,25 @@
 
-function PillarCard({ title }) {
+function PillarCard({ icon, title, description }) {
   return (
-    <div className="flex flex-col px-20 py-12 border-2 border-border">
-      <h3 className="font-title text-primary font-semibold capitalize">
+    <div className="w-100 flex flex-col items-center px-6 py-12 border-2 border-border">
+      {/* Icon */}
+      <div className="flex items-center justify-center w-16 h-16 bg-[#f1f5f9] rounded-full mb-6">
+        <img 
+          src={icon} 
+          alt={`Icône représentant le pilier '${title}'`}
+          className="w-8 h-8 object-contain"
+        />
+      </div>
+
+      {/* Title */}
+      <h3 className="font-title text-primary text-xl font-semibold text-center">
         {title}
       </h3>
+
+      {/* Description */}
+      <p className="font-body text-black text-sm text-center mt-4 leading-6">
+        {description || "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."}
+      </p>
     </div>
   );
 }
--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,12 +1,18 @@
 import HeroPhoto from "../../assets/images/photos/hero_photo.jpg";
 import Button from "../../components/common/Button";
 import PillarCard from "../../components/ui/PillarCard";
+import GraduationCapBlue from "../../assets/images/icons/graduation-cap-blue.svg";
+import GraduationCapWhite from "../../assets/images/icons/graduation-cap-white.svg";
+import TeamBlue from "../../assets/images/icons/team-blue.svg";
+import TeamWhite from "../../assets/images/icons/team-white.svg";
+import BusinessTimeBlue from "../../assets/images/icons/business-time-blue.svg";
+import BusinessTimeWhite from "../../assets/images/icons/business-time-white.svg";
 
 function Home() {
   return (
     <>
       {/*======== Section 1 : Hero  ======== */}
-      <div className="relative z-0 w-full h-96 overflow-hidden lg:h-104">
+      <section className="relative z-0 w-full h-96 overflow-hidden lg:h-104">
         
         {/*==== Sub-Section : Background Image ====*/}
         <img 
@@ -28,23 +34,40 @@ function Home() {
               <Button isPrimary={false} className="">Devenir Étudiant</Button>
             </div>
         </div>
-      </div>
+      </section>
 
       {/*======== Section 2 : Our Three Fundamental Pillars  ======== */}
-      <div className="flex flex-col items-center w-full h-100 bg-white">
-        {/*==== Sub-Section : Title ====*/}
-        <div className="w-full max-w-2xl flex flex-col items-center px-4 mt-10">
-            <h2 className="font-title text-primary text-[32px] font-bold text-center xs:text-[36px] md:text-[40px] leading-10">
-                Nos trois <br /> piliers fondamentaux
-            </h2>
-            <div className="w-20 h-1 bg-secondary mt-4 rounded-full"></div>
+      <section className="w-full py-20 bg-white flex flex-col items-center">
+        <div className="w-[90%] max-w-6xl flex flex-col items-center">
+          {/*==== Sub-Section : Title ====*/}
+          <div className="flex flex-col items-center mb-16">
+              <h2 className="font-title text-primary text-[32px] font-bold text-center xs:text-[36px] md:text-[40px] leading-10">
+                  Nos trois <br /> piliers fondamentaux
+              </h2>
+              <div className="w-20 h-1 bg-secondary mt-4 rounded-full"></div>
+          </div>
+          
+          {/*==== Sub-Section : Pillar Cards ====*/}
+          <div className="w-full flex flex-col items-stretch justify-center gap-8 lg:flex-row ">
+            <PillarCard 
+              icon={GraduationCapBlue}
+              title="Formation Métier"
+              description="Nous formons à un métier. Par la pratique pour rendre l’étudant oppérationnel sur le terrain."
+            />
+            <PillarCard 
+              icon={TeamBlue}
+              title="Centré sur l'humain"
+              description="Nous mettons l’homme au coeur de nos formations, afin de fabriquer des professionnels aguerris et resilients."
+            />
+            <PillarCard 
+              icon={BusinessTimeBlue}
+              title="Employabilités des Jeunes"
+              description="Nous formons à l’emploi et à l’auto-emploi, pour combattre le chômage galoppant."
+            />
+          </div>
+
         </div>
-        
-        {/*==== Sub-Section : Pillar Cards ====*/}
-        <div className="flex flex-col items-center justify-around mt-10 lg:flex-row">
-          <PillarCard title={"Formation métier"} />
-        </div>
-      </div>
+      </section>
     </>
   )
 }
