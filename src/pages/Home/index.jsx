4ccd7f2a5f0a9f8650f7081f22996b7756bd48d0
Hermas Francisco <franciscohermas@gmail.com>
2026-03-16 15:17:58 +0100

    wip: create Section 4 from Home page (not finished)

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,4 +1,4 @@
-import HeroPhoto from "../../assets/images/photos/hero_photo.jpg";
+import HeroPhoto from "../../assets/images/photos/imen-building.jpg";
 import Button from "../../components/common/Button";
 import PillarCard from "../../components/ui/PillarCard";
 import GraduationCapBlue from "../../assets/images/icons/graduation-cap-blue.svg";
@@ -8,6 +8,7 @@ import TeamWhite from "../../assets/images/icons/team-white.svg";
 import BusinessTimeBlue from "../../assets/images/icons/business-time-blue.svg";
 import BusinessTimeWhite from "../../assets/images/icons/business-time-white.svg";
 import StatsCounter from "../../components/ui/StatsCounter";
+import SectionTitle from "../../components/ui/SectionTitle";
 
 function Home() {
   return (
@@ -18,12 +19,12 @@ function Home() {
         {/*==== Sub-Section : Background Image ====*/}
         <img 
           src={HeroPhoto} 
-          alt="Deux étudiants de l'IMeN travaillant ensemble sur un projet" 
+          alt="" 
           className="absolute inset-0 w-full h-full object-cover object-[center_25%]"
           loading="eager"
         />
 
-        <div className="absolute inset-0 bg-black/75 flex flex-col items-center justify-center">
+        <div className="absolute inset-0 bg-black/80 flex flex-col items-center justify-center">
             {/*==== Sub-Section : Text ====*/}
             <h1 className="font-title text-white text-3xl font-semibold capitalize text-center px-2 xs:text-4xl md:text-[44px] lg:text-[50px]">
               Premiere école d'expertise numérique
@@ -39,14 +40,11 @@ function Home() {
 
       {/*======== Section 2 : Our Three Fundamental Pillars  ======== */}
       <section className="w-full pt-8 pb-16 bg-white flex flex-col items-center lg:pt-12 lg:pb-20">
-        <div className="w-[90%] max-w-6xl flex flex-col items-center">
+        <div className="w-[90%]">
           {/*==== Sub-Section : Title ====*/}
-          <div className="flex flex-col items-center mb-8 lg:mb-12">
-              <h2 className="font-title text-primary text-2xl font-bold text-center leading-9 md:text-3xl lg:text-[40px]">
-                  Nos trois <br /> piliers fondamentaux
-              </h2>
-              <div className="w-20 h-1 bg-secondary mt-4 rounded-full lg:mt-6"></div>
-          </div>
+          <SectionTitle>
+            Nos trois <br /> piliers fondamentaux
+          </SectionTitle>
           
           {/*==== Sub-Section : Pillar Cards ====*/}
           <div className="w-full flex flex-col items-stretch justify-center gap-8 lg:flex-row ">
@@ -85,7 +83,30 @@ function Home() {
         <StatsCounter number="+100" title="Partenaires" />
       </section>
 
-      {/*======== Section 4 : ...  ======== */}
+      {/*======== Section 4 : Long-term Training Courses  ======== */}
+      <section className="w-full pt-8 pb-16 flex flex-col items-center lg:pt-14 lg:pb-20">
+        <div className="w-[90%]">
+          {/*==== Sub-Section : Title ====*/}
+          <SectionTitle> 
+            Formations Longues
+          </SectionTitle>
+
+          {/*==== Sub-Section : Training Cards ====*/}
+          <div className="flex flex-row items-center justify-center">
+            <div className="w-100 h-140 flex flex-col border-2 border-border rounded-md overflow-hidden">
+              <img 
+                src="src/assets/images/illustrations/it-programming.png" 
+                alt="Image illustrant deux étudiants de l'IMeN en Programmation Informatique travaillant ensemble sur un projet" 
+                width={626} 
+                height={417} 
+                className="" 
+                loading="lazy" />
+            </div>
+          </div>
+        </div>
+      </section>
+
+      {/*======== Section 5 : ...  ======== */}
       <section className="w-full h-20"></section>
     </main>
   )
