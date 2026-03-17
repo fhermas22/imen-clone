98c0caf603e58bbc0f7ee4edcbd1e568617ee35f
Hermas Francisco <franciscohermas@gmail.com>
2026-03-17 12:14:29 +0100

    feat(ui): create TrainingCard component

--- src/components/ui/TrainingCard/index.jsx
+++ src/components/ui/TrainingCard/index.jsx
@@ -1,6 +1,9 @@
-function TrainingCard({ title, description, imageSrc }) {
+import { Link } from "react-router-dom";
+
+function TrainingCard({ title, description, imageSrc, category, categoryLink }) {
   return (
     <div className="w-full flex flex-col border-2 border-border rounded-md overflow-hidden lg:w-100">
+        {/* Training Illustration */}
       <img 
         src={imageSrc}
         alt={`Image illustrant la formation ${title}`} 
@@ -9,9 +12,19 @@ function TrainingCard({ title, description, imageSrc }) {
         className="" 
         loading="lazy" 
       />
-      <div className="p-4 flex flex-col gap-2">
-        <h3 className="font-title text-primary text-xl font-semibold uppercase">{title}</h3>
-        <p className="text-black text-sm">{description}</p>
+      <div className="pt-8 pb-10 px-9 flex flex-col">
+        {/* Training Category */}
+        <span className="w-40 px-5 py-1 bg-secondary text-center rounded-sm hover:scale-102 group transition-all duration-500 ease-in-out">
+            <Link to={categoryLink} className="text-black text-sm capitalize group-hover:text-white transition-colors duration-500 ease-in-out">
+                {category}
+            </Link>
+        </span>
+
+        {/* Training Title */}
+        <h3 className="font-title text-primary text-xl font-semibold uppercase pt-5 pb-3">{title}</h3>
+
+        {/* Training Description */}
+        <p className="text-black text-sm leading-6">{description}</p>
       </div>
     </div>
   );
