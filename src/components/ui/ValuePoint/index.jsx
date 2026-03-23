2f0aabacae4def3257f7dba9c95d8c2809185f02
Hermas Francisco <franciscohermas@gmail.com>
2026-03-23 15:45:43 +0100

    feat(ui): apply mobile and tablet responsivity on ValuePoint component

--- src/components/ui/ValuePoint/index.jsx
+++ src/components/ui/ValuePoint/index.jsx
@@ -2,7 +2,7 @@ function ValuePoint({ icon, text }) {
     return (
         <div className="flex flex-row items-center gap-4">
             <img src={icon} alt="Icône illustrant le point abordé" width={30} />
-            <p className="font-body text-black text-lg w-[26vw]">
+            <p className="font-body text-black text-base w-[80vw] md:text-lg lg:w-[26vw]">
                 {text}
             </p>
         </div>
