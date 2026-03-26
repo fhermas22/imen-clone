8dd7f0fd6d56bf0dc75abf8fe4e6787cfd9ec16d
Hermas Francisco <franciscohermas@gmail.com>
2026-03-26 11:52:38 +0100

    feat(ui): update TrainingCard component
    Implement logic to prevent empty category data.

--- src/components/ui/TrainingCard/index.jsx
+++ src/components/ui/TrainingCard/index.jsx
@@ -17,14 +17,16 @@ function TrainingCard({ title, description, imageSrc, link, category, categoryLi
 
         <div className="pt-5 pb-4 px-4 flex flex-col md:pt-6 md:pb-8 md:px-6 lg:pt-8 lg:pb-10 lg:px-9">
             {/* Training Category */}
-            <span className="inline-flex w-fit px-5 py-1.5 bg-secondary text-center rounded-sm hover:scale-102 group transition-all duration-500 ease-in-out">
-                <Link to={categoryLink} className="text-black text-sm capitalize whitespace-nowrap group-hover:text-white transition-colors duration-500 ease-in-out">
-                    {category}
-                </Link>
-            </span>
+            {category == null ? null : (
+                <span className="inline-flex w-fit px-5 py-1.5 bg-secondary text-center rounded-sm hover:scale-102 group transition-all duration-500 ease-in-out">
+                    <Link to={categoryLink} className="text-black text-sm capitalize whitespace-nowrap group-hover:text-white transition-colors duration-500 ease-in-out">
+                        {category}
+                    </Link>
+                </span>
+            )}
 
             {/* Training Title */}
-            <Link to={link} className="font-title text-primary text-xl font-semibold uppercase pt-5 pb-3 hover:text-secondary transition duration-500 ease-in-out">
+            <Link to={link} className={`font-title text-primary text-xl font-semibold uppercase pb-3 hover:text-secondary transition duration-500 ease-in-out ${category == null ? "pt-0" : "pt-5"}`}>
                 {title}
             </Link>
 
