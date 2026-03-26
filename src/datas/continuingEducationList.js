33bfe004c874c18a7f01cea3bcb11d73f1f3e7cb
Hermas Francisco <franciscohermas@gmail.com>
2026-03-26 11:50:29 +0100

    chore: create static data file for Continuing Education courses

---
+++ src/datas/continuingEducationList.js
@@ -0,0 +1,25 @@
+{/*== Photo & Illustration Imports  == */}
+import WebDevelopmentIllustration from "../assets/images/illustrations/continuing.education/web-development.jpeg";
+import ComputerMaintenanceIllustration from "../assets/images/illustrations/continuing.education/computer-maintenance.jpg";
+import WebDesignIllustration from "../assets/images/illustrations/continuing.education/web-design.jpeg";
+
+export const continuingEducationList = [
+    {
+        id: 1,
+        title: "Formation en Développement Web",
+        description: "Cette formation est destinée aux personnes en quête d’emploi, en situation de transition professionnelle, aux adultes qui souhaitent approfondir leurs connaissances dans le domaine du Développement Web et obtenir un certificat.",
+        imageSrc: WebDevelopmentIllustration,
+    },
+    {
+        id: 2,
+        title: "Maintenance Informatique",
+        description: "Cette formation est destinée aux personnes en quête d’emploi, en situation de transition professionnelle, aux adultes qui souhaitent approfondir leurs connaissances dans le domaine de la Maintenance Informatique et obtenir un certificat.",
+        imageSrc: ComputerMaintenanceIllustration,
+    },
+    {
+        id: 3,
+        title: "Web Design",
+        description: "Cette formation est destinée aux personnes en quête d’emploi, en situation de transition professionnelle, aux adultes qui souhaitent approfondir leurs connaissances dans le domaine du Web Design et obtenir un certificat.",
+        imageSrc: WebDesignIllustration,
+    }
+]
