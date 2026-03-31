29896722f30e0988969431f6952db5830d4c52ee
Hermas Francisco <franciscohermas@gmail.com>
2026-03-31 10:49:25 +0100

    wip: create Footer layout component (not finished | 2)

--- src/components/layout/Footer/index.jsx
+++ src/components/layout/Footer/index.jsx
@@ -1,4 +1,5 @@
 import LogoDark from "../../../assets/images/logo/imen_logo_dark.png";
+import ChevronRightWhite from "../../../assets/images/icons/chevron-right-white.svg";
 
 function Footer() {
     return (
@@ -10,16 +11,21 @@ function Footer() {
                 {/* Newsletter */}
                 <div className="flex flex-col items-start">
                     <h3 className="font-title text-primary text-lg font-bold text-center md:text-xl lg:text-2xl">Newsletter</h3>
-                    <p className="font-body text-black text-base my-6">
+                    <p className="font-body text-black text-sm my-6">
                         Restez en contact avec nous.
                     </p>
-                    <div className="">
-                        <input type="email" name="email" placeholder="Entrez votre adresse email" className="pl-6 pr-20 py-4 text-sm border-2 border-input"/>
+                    <div className="flex justify-between gap-4 pl-6 pr-2 py-2 border border-input rounded-sm focus-within:outline-2 focus-within:outline-primary focus-within:outline-offset-2">
+                        <input type="email" name="email" id="email" placeholder="Entrez votre adresse email" className="w-64 text-input-text text-xs border-none focus:outline-none"/>
+                        <button type="submit" className="w-10 h-10 flex items-center justify-center bg-primary rounded-sm transition duration-500 ease-in-out hover:cursor-pointer active:scale-90">
+                            <img src={ChevronRightWhite} alt="Soumettre" width={30} height={30}/>
+                        </button>
                     </div>
                 </div>
 
                 {/* Contact Us */}
-                <div></div>
+                <div className="flex flex-col items-start">
+                    <h3 className="font-title text-primary text-lg font-bold text-center md:text-xl lg:text-2xl">Contactez-nous</h3>
+                </div>
             </div>
         </footer>
     );
