727a59f448cb3c653eba4e0a3c505a8fa5feb909
Hermas Francisco <franciscohermas@gmail.com>
2026-04-03 16:07:11 +0100

    feat(ui): apply mobile and tablet responsivity on Footer layout

--- src/components/layout/Footer/index.jsx
+++ src/components/layout/Footer/index.jsx
@@ -19,21 +19,27 @@ function Footer() {
     const currentYear = new Date().getFullYear();
 
     return (
-        <footer className="w-full flex flex-col pt-25 bg-footer">
-            <div className="flex flex-col items-center justify-center gap-40 mb-12 lg:flex-row lg:items-start">
+        <footer className="w-full flex flex-col pt-15 bg-footer lg:pt-25">
+            <div className="flex flex-col items-center justify-center gap-14 px-3 mb-12 md:gap-10 md:flex-row md:items-start lg:gap-40">
                 {/* Logo */}
-                <img src={LogoDark} alt="Logo de l'IMeN" className="w-62"/>
+                <div className="flex flex-row items-center justify-center">
+                    <img src={LogoDark} alt="Logo de l'IMeN" className="w-62 md:w-48 lg:w-62"/>
+                </div>
 
                 {/* Newsletter */}
-                <div className="flex flex-col items-start">
+                <div className="w-[90vw] flex flex-col items-start md:w-max">
                     <h3 className="font-title text-primary text-lg font-bold text-center md:text-xl lg:text-2xl">Newsletter</h3>
 
-                    <p className="font-body text-black text-sm my-6">
+                    <p className="font-body text-black text-sm mt-2 mb-4 lg:my-6">
                         Restez en contact avec nous.
                     </p>
 
                     <div className="flex justify-between gap-4 pl-6 pr-2 py-2 border border-input rounded-sm focus-within:outline-2 focus-within:outline-primary focus-within:outline-offset-2">
-                        <input type="email" name="email" id="email" placeholder="Entrez votre adresse email" className="w-64 font-body text-input-text text-xs bg-transparent border-none focus:outline-none focus:text-slate-800"/>
+                        <input 
+                            type="email" name="email" id="email" 
+                            placeholder="Entrez votre adresse email" 
+                            className="w-[60vw] font-body text-input-text text-xs bg-transparent border-none focus:outline-none focus:text-slate-800 xs:w-[65vw] md:w-[14vw] lg:w-[14vw]"
+                        />
                         <button type="submit" className="w-10 h-10 flex items-center justify-center bg-primary rounded-sm transition duration-500 ease-in-out hover:cursor-pointer active:scale-90">
                             <img src={ChevronRightWhite} alt="Soumettre" width={30} height={30}/>
                         </button>
@@ -69,34 +75,34 @@ function Footer() {
                 </div>
 
                 {/* Contact Us */}
-                <div className="flex flex-col items-start">
+                <div className="w-[90vw] flex flex-col items-start md:w-max">
                     <h3 className="font-title text-primary text-lg font-bold text-center md:text-xl lg:text-2xl">Contactez-nous</h3>
 
-                    <div className="flex flex-col gap-4 my-6">
+                    <div className="flex flex-col gap-4 my-4 lg:my-6">
                         <span className="flex items-center justify-items-start gap-2">
-                            <img src={LocationPrimary} alt="Icône d'adresse" width={26} height={26}/>
-                            <p className="font-body text-primary text-sm font-medium">
+                            <img src={LocationPrimary} alt="Icône d'adresse" className="w-6.5 h-6.5 md:w-5 md:h-5 lg:w-6.5 lg:h-6.5" />
+                            <p className="font-body text-primary text-sm font-medium md:text-xs lg:text-sm">
                                 Adresse :
                             </p>
-                            <a href="https://share.google/jI4xVsooBskwFc7iA" target="_blank" rel="noopener noreferrer" className="font-body text-black text-sm">
+                            <a href="https://share.google/jI4xVsooBskwFc7iA" target="_blank" rel="noopener noreferrer" className="font-body text-black text-xs xs:text-sm md:text-xs lg:text-sm">
                                 Bénin, Cotonou, Vedoko, BP 1250
                             </a>
                         </span>
                         <span className="flex items-center justify-items-start gap-2">
-                            <img src={PhoneNumberPrimary} alt="Icône de téléphone" width={26} height={26}/>
-                            <p className="font-body text-primary text-sm font-medium">
+                            <img src={PhoneNumberPrimary} alt="Icône de téléphone" className="w-6.5 h-6.5 md:w-5 md:h-5 lg:w-6.5 lg:h-6.5" />
+                            <p className="font-body text-primary text-sm font-medium md:text-xs lg:text-sm">
                                 Téléphone :
                             </p>
-                            <a href="tel:+2290195922345" className="font-body text-black text-sm">
+                            <a href="tel:+2290195922345" className="font-body text-black text-xs xs:text-sm md:text-xs lg:text-sm">
                                 +229 01 95 92 23 45
                             </a>
                         </span>
                         <span className="flex items-center jub-10stify-items-start gap-2">
-                            <img src={EmailPrimary} alt="Icône d'email" width={26} height={26}/>
-                            <p className="font-body text-primary text-sm font-medium">
+                            <img src={EmailPrimary} alt="Icône d'email" className="w-6.5 h-6.5 md:w-5 md:h-5 lg:w-6.5 lg:h-6.5" />
+                            <p className="font-body text-primary text-sm font-medium md:text-xs lg:text-sm">
                                 Email :
                             </p>
-                            <a href="mailto:info@imen.school" className="font-body text-black text-sm">
+                            <a href="mailto:info@imen.school" className="font-body text-black text-xs xs:text-sm md:text-xs lg:text-sm">
                                 info@imen.school
                             </a>
                         </span>
@@ -105,8 +111,8 @@ function Footer() {
             </div>
 
             {/* Copyright */}
-            <div className="flex items-center justify-center py-6 border-t border-footer-border">
-                <p className="font-body text-black text-sm">
+            <div className="flex items-center justify-center py-7 border-t border-footer-border lg:py-6">
+                <p className="font-body text-black text-xs lg:text-sm">
                     Copyright IMeN © {currentYear} | Tout droits réservés.
                 </p>
             </div>
