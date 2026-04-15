dc365e48c2e96c9ea6109225046f12500ac6d350
Hermas Francisco <franciscohermas@gmail.com>
2026-04-15 10:22:59 +0100

    refactor(data): change target_audience value to null for long-term trainings to prevent true logic miss-match

--- src/datas/trainingList.js
+++ src/datas/trainingList.js
@@ -18,7 +18,7 @@ export const trainingList = [
     presentation: "La filière Big Data et Intelligence Artificielle forme des experts capables de traiter, analyser et exploiter de grandes quantités de données pour en extraire des informations stratégiques et des modèles prédictifs. Elle couvre des domaines tels que la science des données, l'apprentissage automatique, le traitement du langage naturel et la visualisation de données, permettant ainsi de développer des solutions innovantes pour la prise de décision et l'automatisation des tâches. Cette formation répond aux besoins croissants des entreprises souhaitant optimiser leurs performances à l'aide des technologies avancées de l'IA et du Big Data.",
     goals: null,
     openings: "En choisissant ce parcours de formation, vous pouvez exercer plusieurs métiers dont entre autres : Data Scientist ; Data Engineer ; Ingénieur en Intelligence Artificielle ; Analyste Big Data ; Consultant en transformation digitale ; Développeur en Machine Learning, etc.",
-    target_audience: "",
+    target_audience: null,
     materials: [
       "Ordinateur portable", 
       "Clé USB ou disque dur externe", 
@@ -39,7 +39,7 @@ export const trainingList = [
     presentation: "La filière Programmation Informatique forme des professionnels capables de concevoir, développer et optimiser des logiciels, applications web et mobiles ainsi que des systèmes informatiques. Elle couvre les langages de programmation essentiels (comme Python, Java, C++, et JavaScript), les principes d'algorithmique, et les méthodes de développement collaboratif. Grâce à cette formation, les étudiants acquièrent des compétences en analyse et en résolution de problèmes techniques, en respectant les standards de qualité et de sécurité. Cette filière répond aux besoins croissants du marché pour des développeurs polyvalents et spécialisés, indispensables dans tous les secteurs du numérique.",
     goals: null,
     openings: "En choisissant ce parcours de formation, vous pouvez exercer plusieurs métiers dont entre autres : Développeur web ; Développeur d'applications mobiles; Développeur logiciel ; Ingénieur DevOps; Ingénieur système et réseaux ; Analyste programmeur; Consultant en technologie, etc.",
-    target_audience: "",
+    target_audience: null,
     materials: [
       "Ordinateur portable", 
       "Clé USB ou disque dur externe", 
@@ -60,7 +60,7 @@ export const trainingList = [
     presentation: "La filière Sécurité Informatique forme des spécialistes capables de protéger les systèmes d'information, les réseaux et les données contre les cybermenaces. Elle couvre les domaines de la cybersécurité, de la cryptographie, de l'analyse de risques, ainsi que des techniques de défense et de réponse aux incidents. Cette formation enseigne aux étudiants comment détecter, analyser et contrer les menaces de sécurité tout en assurant la confidentialité, l'intégrité et la disponibilité des informations. Dans un contexte de hausse des cyberattaques, cette filière est essentielle pour garantir la sécurité des entreprises et des organisations.",
     goals: null,
     openings: "En choisissant ce parcours de formation, vous pouvez exercer plusieurs métiers dont entre autres : Analyste en cybersécurité ; Ingénieur en sécurité informatique ; Responsable de la sécurité des systèmes d'information (RSSI) ; Pentester (testeur d'intrusion) ; Consultant en sécurité informatique ; Auditeur en sécurité informatique ; Cryptographe, etc.",
-    target_audience: "",
+    target_audience: null,
     materials: [
       "Ordinateur portable", 
       "Clé USB ou disque dur externe", 
@@ -82,7 +82,7 @@ export const trainingList = [
     presentation: "La formation en Cybersécurité est conçue pour transformer les étudiants en véritables remparts contre la cybercriminalité. Ce cursus intensif se concentre sur la mise en œuvre de stratégies de défense avancées, la surveillance des réseaux via les SOC (Security Operations Center), et la gestion des vulnérabilités. Vous apprendrez à maîtriser les outils de détection d'intrusion, à mener des investigations numériques (forensics) et à élaborer des plans de continuité d'activité. L'objectif est de former des profils opérationnels capables d'évoluer dans un paysage technologique où la menace est constante, en alliant compétences techniques pointues et conformité réglementaire.",
     goals: null,
     openings: "En choisissant ce parcours de formation, vous pouvez exercer plusieurs métiers dont entre autres : Analyste SOC (Security Operations Center) ; Ingénieur en sécurité informatique ; Enquêteur numérique ; Responsable de la sécurité des systèmes d'information (RSSI), etc.",
-    target_audience: "",
+    target_audience: null,
     materials: [
       "Ordinateur portable performant (8Go RAM min.)",
       "Clé USB de sécurité",
@@ -106,7 +106,7 @@ export const trainingList = [
     presentation: "La filière Installation et Maintenance Informatique forme des techniciens qualifiés pour assurer la mise en place, la configuration, l'entretien et la réparation des systèmes informatiques et réseaux. Elle couvre les aspects techniques du matériel (hardware), les logiciels (software), ainsi que la gestion des réseaux et des périphériques. Les étudiants apprennent à diagnostiquer et résoudre des pannes, à effectuer des installations et des mises à jour, et à garantir le bon fonctionnement des infrastructures informatiques. Cette formation est essentielle pour soutenir les entreprises et organisations dans la continuité de leurs opérations informatiques.",
     goals: null,
     openings: "En choisissant ce parcours de formation, vous pouvez exercer plusieurs métiers dont entre autres : Technicien de maintenance informatique ; Technicien réseau ; Administrateur système junior ; Technicien support informatique ; Responsable technique en centre de service ; Installateur de matériel informatique, etc.",
-    target_audience: "",
+    target_audience: null,
     materials: [
       "Ordinateur portable",
       "Clé USB ou disque dur externe",
@@ -128,7 +128,7 @@ export const trainingList = [
     presentation: "La filière Développement Web & Mobile forme des professionnels capables de concevoir, développer et maintenir des applications web et mobiles performantes et sécurisées. Elle couvre les principaux langages et frameworks (HTML, CSS, JavaScript, React, Flutter, etc.), les bases de données, ainsi que les techniques de design UX/UI. Les étudiants apprennent à créer des applications dynamiques, intuitives et adaptées aux besoins des utilisateurs sur diverses plateformes. Cette formation répond aux exigences croissantes du marché du numérique, où les solutions en ligne et mobiles sont omniprésentes.",
     goals: null,
     openings: "En choisissant ce parcours de formation, vous pouvez exercer plusieurs métiers dont entre autres : Développeur web ; Développeur mobile ; Intégrateur web ; Designer UX/UI ; Développeur full-stack ; Chef de projet web, etc.",
-    target_audience: "",
+    target_audience: null,
     materials: [
       "Ordinateur portable",
       "Clé USB ou disque dur externe",
@@ -149,7 +149,7 @@ export const trainingList = [
     presentation: "Le Motion Design, ou design animé, est une discipline qui allie graphisme et animation pour créer des contenus visuels dynamiques. Cette technique utilise des éléments graphiques animés, des typographies en mouvement et des effets visuels pour communiquer des idées, des récits ou des messages à travers des vidéos, des publicités, des présentations et des contenus numériques. Le Motion Design est largement utilisé dans des domaines tels que la publicité, le cinéma, la télévision et les médias numériques.",
     goals: null,
     openings: "En choisissant ce parcours de formation, vous pouvez exercer plusieurs métiers dont entre autres : Motion Designer ; Animateur ; Directeur Artistique ; Concepteur 3D; Graphiste Multimédia ; Vidéaste ; Designer d'Interface Animée, etc.",
-    target_audience: "",
+    target_audience: null,
     materials: [
       "Ordinateur portable", 
       "Clé USB ou disque dur externe", 
@@ -171,7 +171,7 @@ export const trainingList = [
     presentation: "Le Design UI/UX (Interface Utilisateur / Expérience Utilisateur) est un domaine en plein essor, centré sur la création de produits numériques intuitifs et esthétiques. Les designers UI/UX s'assurent que les interfaces des applications et sites web sont non seulement visuellement attrayantes, mais aussi faciles à naviguer pour offrir une expérience utilisateur optimale.",
     goals: null,
     openings: "En choisissant ce parcours de formation, vous pouvez exercer plusieurs métiers dont entre autres : Designer UI/UX ; UX Researcher ; Product Designer ; Consultant en Design ; Responsable de la conception, etc.",
-    target_audience: "",
+    target_audience: null,
     materials: [
       "Ordinateur portable",
       "Clé USB ou disque dur externe",
