581e56da975347e7cc1dc5fdced11945a3b7d118
Hermas Francisco <franciscohermas@gmail.com>
2026-04-28 10:50:14 +0100

    chore(data): create the static data list of programs with detailed descriptions

---
+++ src/datas/programList.js
@@ -0,0 +1,36 @@
+export const programList = [
+    {
+        id: 1,
+        title: "Programme d'Insertion Professionnelle",
+        contents: [
+            "Le **Programme d'Insertion Professionnelle (PIP)** est une initiative sur mesure conçue pour faciliter l’intégration professionnelle des étudiants de l’IMeN. Ce programme vise à les accompagner dans leurs premiers pas en entreprise, leur permettant ainsi de décrocher rapidement un emploi à l’issue de leur formation.",
+            "Animé par des consultants expérimentés et des coachs spécialisés dans le développement de carrière et l’employabilité des jeunes, le PIP propose un parcours complet. Les étudiants y apprennent à valoriser leurs compétences, à maîtriser les techniques de recherche d’emploi, et à produire des candidatures percutantes (CV et lettres de motivation). Une importance particulière est accordée à la préparation aux entretiens d’embauche ainsi qu’à l’assimilation des codes de la culture d’entreprise.",
+            "Le programme adopte une approche concrète et pratique, intégrant des ateliers interactifs, des exercices d’auto-évaluation et des simulations. Ces activités permettent aux participants de renforcer leur confiance et leur adaptabilité face aux exigences du marché de l’emploi.",
+            "En participant au PIP, les étudiants sortent pleinement équipés sur le plan technique, psychologique et professionnel pour réussir dans leur domaine et décrocher leur emploi de rêve."
+        ] 
+    },
+    {
+        id: 2,
+        title: "Programme de Formation en Entrepreneuriat",
+        contents: [
+            "Le **Programme de Formation en Entrepreneuriat (PFE)** est un programme dédié aux étudiants porteurs de projet ou aspirants à l’entrepreneuriat. Il s’agit d’un accompagnement offert après le parcours de formation qui vise à préparer le futur entrepreneur aux challenges qui l’attendent. Le but de ce programme est de former et d’outiller efficacement les jeunes porteurs de projet à lancer, amorcer et gérer leur entreprise. Ils sont dopés du ‘’Mindset’’ d’entrepreneur et capables de limiter les risques d’un échec lamentable pour assurer le succès de leur projet.",
+            "Les formations de ce programme sont assurées par des consultants, coach et cabinets partenaires de l’IMeN. Elles impliquent des ateliers pratiques, des workshops et des séminaires pour transmettre aux étudiants et futures entrepreneurs le leadership et les compétences nécessaires indispensables à leur succès."
+        ]
+    },
+    {
+        id: 3,
+        title: "La Foire de l’Emploi",
+        contents: [
+            "Cet évènement qui se déroule sur plusieurs jours est pensé par l’IMeN pour ses étudiants certifiés et ses entreprises partenaires. Il a pour but de créer le contact entre les deux parties, donnant l’occasion des entretiens et du recrutement des nouveaux certifiés.",
+            "La **Foire de l’Emploi** est un rendez-vous incontournable pour les étudiants certifiés de l’IMeN, leur offrant une plateforme privilégiée pour rencontrer des employeurs potentiels et décrocher des opportunités professionnelles.",
+        ]
+    },
+    {
+        id: 4,
+        title: "IMeN Boost Days",
+        contents: [
+            "Les **IMeN Boost Days** sont des séances de partage d’expériences, des séminaires thématiques et autres pour permettre aux étudiants d’apprendre à se vendre, à vendre et se préparer à affronter la vie professionnelle avec sérénité.",
+            "Ces programmes sont offerts gratuitement, garantissant à chaque étudiant un soutien concret pour sa carrière, qu’il choisisse de rejoindre une entreprise ou de se lancer dans l’aventure entrepreneuriale."
+        ]
+    }
+]
