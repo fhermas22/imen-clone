ccfef0002e47f0f896167ac39bd9087339567e83
Hermas Francisco <franciscohermas@gmail.com>
2026-05-07 11:14:39 +0100

    chore(data): create the static data list for detailed informations about the IMeN school

---
+++ src/datas/schoolInfo.js
@@ -0,0 +1,71 @@
+export const schoolInfo = [
+    {
+        id: 1,
+        title: "Qui Sommes-Nous ?",
+        introductionText: null,
+        contents: [
+            "Fondée en 2022, l’**Institut des Métiers du Numérique** se positionne comme la première école d’informatique et de programmation web d’Afrique de l’Ouest. Dans un monde où la transformation numérique redéfinit les enjeux économiques et sociaux, IMeN répond aux défis actuels en offrant des formations novatrices et de haute qualité. Alors que les systèmes éducatifs traditionnels peinent à suivre le rythme du marché de l’emploi, l’école forme des professionnels prêts à intégrer des secteurs en constante évolution.",
+            "L’approche pédagogique de l’IMeN, centrée sur la pratique et l’innovation, vise à développer des compétences concrètes, immédiatement applicables sur le terrain. Chaque étudiant bénéficie d’un parcours immersif qui place l’apprentissage au cœur de situations réelles, renforçant ainsi son employabilité. En plus des domaines de l'informatique et de la programmation web, IMeN propose des formations dans des secteurs stratégiques tels que l’audiovisuel et le design graphique.",
+            "Cette école ne se contente pas de former des techniciens, elle façonne des leaders capables de transformer l'Afrique en un acteur majeur de l'économie numérique mondiale. IMeN, véritable tremplin pour la réussite professionnelle, prépare ses étudiants à un avenir brillant dans un environnement global en pleine mutation."
+        ]
+    },
+    {
+        id: 2,
+        title: "Pourquoi l'IM~~e~~N ?",
+        contents: [
+            "La création de l'Institut des Métiers du Numérique trouve son origine dans l’observation d’un écart croissant entre les formations traditionnelles et les besoins réels des entreprises en matière de compétences. Dans un monde de plus en plus digitalisé, où le numérique transforme profondément tous les secteurs, il devient évident que les cursus classiques ne suffisent plus. Ces formations, souvent déconnectées des réalités du marché, contribuent à la formation de milliers de diplômés sans emploi, dont les compétences ne correspondent plus aux attentes des recruteurs.",
+            "Face à ce constat, l’IMeN a pour mission de combler ce vide en proposant des programmes de formation spécialisés dans les métiers du numérique. Ces métiers, en constante évolution, sont désormais incontournables sur le marché du travail. L’objectif de l’IMeN est de former des professionnels hautement qualifiés, dont les compétences sont directement alignées avec les besoins spécifiques des entreprises, tout en les préparant à relever les défis entrepreneuriaux actuels et futurs. Ainsi, l'Institut joue un rôle clé dans la préparation de la prochaine génération de talents numériques, capables de contribuer efficacement à l’économie numérique de demain.",
+        ],
+        introductionText: "Voici en résumé en cinq (05) points, pour lesquels **IMeN** reste le meilleur choix :",
+        detailedContents: [
+            {
+                id: 1,
+                title: "Pour exceller dans votre domaine",
+                content: "Nos formateurs, experts reconnus, façonnent votre avenir professionnel avec des compétences de pointe."
+            },
+            {
+                id: 2,
+                title: "Pour une formation 100% pratique",
+                content: "Nos cours sont conçus pour vous rendre immédiatement opérationnel sur le terrain."
+            },
+            {
+                id: 3,
+                title: "Pour un avenir professionnel sécurisé",
+                content: "Avec un stage garanti et un accompagnement personnalisé vers l’emploi, nos certifiés rejoignent un réseau de plus de 200 entreprises partenaires."
+            },
+            {
+                id: 4,
+                title: "Apprenez l’anglais pour vous ouvrir au monde",
+                content: "Tous nos étudiants profitent d’un accès gratuit à l’**IMeN English Academy**, pour maîtriser l’anglais et élargir leurs opportunités à l’international."
+            },
+            {
+                id: 5,
+                title: "Pour un certificat reconnu",
+                content: "Votre formation est validée par un certificat reconnu par l’État, ouvrant des portes tant au niveau national qu’international."
+            }
+        ]
+    },
+    {
+        id: 3,
+        title: "Nos Trois Piliers Fondamentaux",
+        contents: [],
+        introductionText: null,
+        detailedContents: [
+            {
+                id: 1,
+                title: "Formation métier par la pratique",
+                content: "Nous formons nos étudiants à des métiers concrets, avec un focus sur la pratique pour qu’ils soient immédiatement opérationnels à la fin de leur formation."
+            },
+            {
+                id: 2,
+                title: "Focus sur l’humain",
+                content: "Nous plaçons l’humain au centre de notre démarche, forgeant des professionnels résilients, prêts à réussir dans leur vie socioprofessionnelle."
+            },
+            {
+                id: 3,
+                title: "Accompagnement vers l’emploi et l’auto-emploi",
+                content: "Nous formons nos étudiants pour l’emploi ou l’auto-emploi, les accompagnant dans la création de leur propre activité ou dans le démarrage d’une carrière en entreprise, afin de lutter efficacement contre le chômage."
+            }
+        ]
+    }
+] 
